9ecf44f0f6c15aaa0d90278105f2f29aab94bf9f
Lakshan1806 <lakshanuthayan@gmail.com>
2025-08-07 12:34:31 +0530

    feat: Add DriveHeroSection component to Drive page
    Introduces a new DriveHeroSection component displaying a drive image and integrates it into the Drive page layout. Also adds the drive image asset.

--- app-customer-web/src/pages/Drive.jsx
+++ app-customer-web/src/pages/Drive.jsx
@@ -1,5 +1,11 @@
+import DriveHeroSection from "../components/Drive/DriveHeroSection";
+
 function Drive() {
-  return <div className="flex h-full flex-col">Drive</div>;
+  return (
+    <div className="flex h-full min-h-0 w-full snap-y snap-mandatory flex-col overflow-y-auto scroll-smooth">
+      <DriveHeroSection />
+    </div>
+  );
 }
 
 export default Drive;
