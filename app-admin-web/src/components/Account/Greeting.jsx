64dc7ad1d693e10a97a68fb5a5849a63029d54d2
Lakshan1806 <lakshanuthayan@gmail.com>
2025-08-20 00:09:44 +0530

    feat: Add personalized greeting to dashboard
    Introduces a Greeting component that displays the admin's photo and username from localStorage. The Greeting is now rendered on the Dashboard for a more personalized user experience.

--- app-admin-web/src/components/Account/Greeting.jsx
+++ app-admin-web/src/components/Account/Greeting.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
+const adminData = JSON.parse(localStorage.getItem("adminData") || "{}");
+console.log("adminData:", adminData);
 function Greeting() {
   return (
-    <div>Greeting</div>
-  )
+    <div className="absolute top-0 right-0 flex items-center gap-4 px-[50px] py-[20px]">
+      <img
+        className="h-12 w-12 rounded-full object-cover"
+        src={adminData.photo}
+        alt="Urban Move Logo"
+      />
+      <div className="flex flex-col">
+        <p className="text-sm font-semibold">Welcome back!</p>
+        <p className="text-lg font-bold">{adminData.username}</p>
+      </div>
+    </div>
+  );
 }
 
-export default Greeting
+export default Greeting;
