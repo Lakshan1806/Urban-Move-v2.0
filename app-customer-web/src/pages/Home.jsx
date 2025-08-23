1572ef07976ad39c8216d4fa7bd679213bc9ea4e
Lakshan1806 <lakshanuthayan@gmail.com>
2025-08-23 09:43:36 +0530

    feat: Add no-scrollbar utility and update Home page layout
    Introduces a CSS utility class to hide scrollbars and applies it to the Home page container. Also updates Home page imports to use lowercase 'home' directory, adds Footer and HomeActionCards_Copy components to the layout.

--- app-customer-web/src/pages/Home.jsx
+++ app-customer-web/src/pages/Home.jsx
@@ -1,11 +1,15 @@
-import HomeActionCards from "../components/Home/HomeActionCards";
-import Promotions from "../components/Home/Promotions";
+import Footer from "../components/Footer";
+import HomeActionCards from "../components/home/HomeActionCards";
+import HomeActionCards_Copy from "../components/home/HomeActionCards_Copy";
+import Promotions from "../components/home/Promotions";
 
 function Home() {
   return (
-    <div className="flex h-full min-h-0 w-full snap-y snap-mandatory flex-col overflow-y-auto scroll-smooth">
+    <div className="no-scrollbar flex h-full min-h-0 w-full snap-y snap-mandatory flex-col overflow-y-auto scroll-smooth">
+      <HomeActionCards_Copy />
       <HomeActionCards />
       <Promotions />
+      <Footer />
     </div>
   );
 }
